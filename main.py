0f1abae99dfa1dec26abc3f2e10778365295d48c
wjgusen98 <jhd6290@gmail.com>
2026-01-29 11:14:38 +0900

    Add MCP client example with integration with langchain

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+stdio_sever_params = StdioServerParameters(
+    command="python",
+    args=["/Users/user/mcp_course/servers/math_server.py"]
+)
+
 async def main ():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_sever_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            print(tools)
+            
+            agent = create_agent(llm,tools)
+            result = await agent.ainvoke({"messages" : [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
